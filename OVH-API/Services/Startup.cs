ebbf45fdeb1b516afb7dce2420b189afcdda150b
Texas Red <texas@texasred.co.uk>
2021-10-23 00:58:47 +0100

    Minor Clean Up

--- OVH-API/Services/Startup.cs
+++ OVH-API/Services/Startup.cs
@@ -11,20 +11,11 @@ namespace OVHAPI.Services
 {
     public class Startup
     {
-        #region Look & Feel
-
-        //[DllImport("kernel32.dll", ExactSpelling = true)]
-        //private static extern IntPtr GetConsoleWindow();
-
-        //[DllImport("user32.dll", EntryPoint = "SetWindowPos")]
-        //private static extern IntPtr SetWindowPos(IntPtr hWnd, int hWndInsertAfter, int x, int Y, int cx, int cy, int wFlags);
-        #endregion
         public Startup(string[] arguments)
         {
             Console.Title = "Nebula Mods Inc. [OVH API] ~ Nebula";
             if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
             {
-                //SetWindowPos(GetConsoleWindow(), 0, 550, 300, 0, 0, 0x0001);
                 Console.SetWindowSize(85, 20);
                 Console.SetBufferSize(85, 20);
             }
@@ -40,14 +31,14 @@ namespace OVHAPI.Services
                 var provider = services.BuildServiceProvider();
                 provider.GetRequiredService<DatabaseService>().Database.Migrate();
                 await provider.GetRequiredService<CommandService>().StartAsync();
-                //
                 await Task.Delay(-1);
             }
-            catch (Exception error)
+            catch (Exception)
             {
                 
             }
         }
+
         private static void ConfigureServices(IServiceCollection services)
         {
             services.AddSingleton<DatabaseService>()
